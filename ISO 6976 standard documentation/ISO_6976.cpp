e319356f3c0b60f1620d4d38e309176533ca12de
Orhan <102863731+unrasyonel@users.noreply.github.com>
2024-06-27 12:50:43 +0300

    Relocationg ISO_6976.cpp to ISO 6976 standard documentation/ISO_6976.cpp
    tidying up.

--- ISO 6976 standard documentation/ISO_6976.cpp
+++ ISO 6976 standard documentation/ISO_6976.cpp
@@ -1,4 +1,12 @@
-//ISO-6976
+/* ISO-6976
+
+A simple program to calculate various calorific values based on the ISO 6976 standard documentation. A small group project.
+
+2005 release of the document can be obtained from the following link:
+
+https://annas-archive.org/md5/366fd8036a647a100f704adf6599b768
+
+*/
 
 #include <cstdio> 
 #include <cmath>
