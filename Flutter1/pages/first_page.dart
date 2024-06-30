0feac221930b15b39a3e80d1952669e1462ab63b
Orhan <102863731+unrasyonel@users.noreply.github.com>
2024-06-30 23:59:36 +0300

    Create first_page.dart

---
+++ Flutter1/pages/first_page.dart
@@ -0,0 +1,27 @@
+import 'package:fitness/pages/second_page.dart';
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("1st Page"),
+        backgroundColor: Colors.teal,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Go to second page"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
