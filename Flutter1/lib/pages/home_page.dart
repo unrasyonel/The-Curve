53c284e714a3d2ed4c73ffe9a0ff587db7aa7f20
Orhan <102863731+unrasyonel@users.noreply.github.com>
2024-07-03 14:07:37 +0300

    Add clickable checbox for tasks.

--- Flutter1/lib/pages/home_page.dart
+++ Flutter1/lib/pages/home_page.dart
@@ -2,7 +2,6 @@ import 'package:fitness/utilities/todo_tile.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-
   const HomePage({super.key});
 
   @override
@@ -10,37 +9,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // List of todo tasks
+  List toDoList = [
+    ["Artvin oyna.", true],
+    ["Daha fazla Artvin oyna.", false],
+    ["Dizlik al.", false],
+    ["Tutorial'ı bitir.", false],
+  ];
+
+  void checkboxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  void createNewTask() {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal[400],
       appBar: AppBar(
         title: Text("TO DO"),
+        centerTitle: true,
         backgroundColor: Colors.teal,
-        
-        
       ),
-
-    body: ListView(
-      children: [
-        TodoTile(
-          taskName: "Artvin oyna.",
-          taskCompleted: true,
-          onChanged: (p0) {},
-        ),
-
-        TodoTile(taskName: "Biraz daha Artvin oyna.",
-         taskCompleted: false, 
-         onChanged: (p1) {}),
-        
-        TodoTile(taskName: "DAHA FAZLA Artvin oyna.",
-         taskCompleted: true, 
-         onChanged: (p1) {}),
-        
-      ],
-
-    ),
-
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkboxChanged(value, index),
+          );
+        },
+      ),
     );
   }
 }
